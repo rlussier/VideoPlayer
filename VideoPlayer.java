bb3e6c2df8afdbebb7ec94ebbf1f769c34102115
Reggie <reganlussier@gmail.com>
2018-11-01 10:51:49 -0500

    Update VideoPlayer.java

--- VideoPlayer.java
+++ VideoPlayer.java
@@ -19,6 +19,14 @@ public class MoviePlayer extends Application {
 	    MediaPlayer player = new MediaPayer(media);
 	    MediaView view = new MediaView(player);
 	    
+	    // System.out.print("media.width: " + media.getWidth());
+	    
+	    final VBoc vbox = new VBox();
+	    Slider slider = new Slider();
+	    vbox.getChildren().add(slider);
+	    
+	    root.getChildren().add(vbox);
+	    root.getChildren().add(view);
 	    media.getWidth();
 	    media.getHeight();
 	    
@@ -30,14 +38,38 @@ public class MoviePlayer extends Application {
 	    player.play();
 	    player.setOnReady(new Runnable()
         {
+            
         @Override 
         public void run() {
-            int w = player.getMedia().getWidth();
+             int w = player.getMedia().getWidth();
              int h = player.getMedia().getHeight();
         
              stage.setMinWidth(w);
              stage.setMinHeight(h);
+             
+             vbox.setMinSize(w, 100);
+             vbox.setTranslateY(h-25);
+             
+             slider.setMin(0.0);
+             slider.setValue(0.0);
+             slider.setMax(player.getTotalDuration().toSeconds());
             }
 	    });
+	    
+	    player.currentTimeProperty().addListener(new ChangeListener<Duration>() {
+	        @Override
+	        public void changed(ObservableValue<? extends Duration> observableValue, Duration duration, duration current) {
+	            slider.setValue(current.toSeconds());
+	        }
+	    });
+	    
+	    slider.setOnMouseClicked(new EventHandler<MouseEvent>() {
+	        @Override 
+	        public void handle(MouseEvent mouseEvent) {
+	            player.seek(Duration.seconds(slider.getValue()));
+	        }
+	    }
+	    // Controller
+	    
     }
 }
